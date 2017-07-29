7c567ca165cd66909025f46be1aa46d24485672d
Carson Rajcan <carajcan@gmail.com>
2017-07-29 17:31:09 -0500

    Add validations to user model

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-  @name="nick"
   end
 
   def help
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
-
-  def find_user(user_id)
-    User.find(user_id)
-  end
-
+  before_save { self.email = email.downcase }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates(:email, presence: true, 
+                    length: {maximum: 255}, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false })
+  has_secure_password
 end
