4a6c90732ccc88569aae44007e89a1674febb35a
Carson Rajcan <carajcan@gmail.com>
2017-08-13 14:54:46 -0500

    continue signup and log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      flash[:success] = "Welcome to Pivot!"
+      Log_in(@user)
+      redirect_to @user
     else
       render 'new' 
     end
