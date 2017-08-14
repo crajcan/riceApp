de0758f813759b843cf384fcd52e4f49f635c738
Carson Rajcan <carajcan@gmail.com>
2017-08-13 19:29:53 -0500

    add user login tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :password, presence: true, length: { minimum: 6 }
-
+  validates :password, presence: true, length: { minimum: 6, maximum: 25 }
   validates :name, presence: true, length: { maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates(:email, presence: true, 
@@ -9,4 +8,10 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false })
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
