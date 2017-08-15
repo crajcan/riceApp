49c5a01137edf57a136a86d59840eaf71db363c9
Carson Rajcan <carajcan@gmail.com>
2017-08-15 00:17:58 -0500

    finish logins

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def log_out
-    session[:user_id] = nil
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def log_out
+    forget(current_user)
+    session[:user_id] = nil
+    @current_user = nil
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
   # Returns true if the given user is the current user
   def current_user?(user)
