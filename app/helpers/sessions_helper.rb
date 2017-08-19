b861b54e26d509e94f2db3ef4ed821fa642f06ab
Carson Rajcan <carajcan@gmail.com>
2017-08-18 20:06:05 -0500

    finish remember logins

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -33,8 +33,8 @@ module SessionsHelper
       @current_user = @current_user || User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id]) 
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
+      if user && user.authenticated?(cookies[:remember_token])
+        Log_in user
         @current_user = user
         end
     end
