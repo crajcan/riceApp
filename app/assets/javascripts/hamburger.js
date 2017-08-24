7ca56af3371bccf867614964b20fd6be552f0e15
Carson Rajcan <carajcan@gmail.com>
2017-08-24 12:55:23 -0500

    enlarge burger

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -1,9 +1,13 @@
+//on render or redirect
 $(document).on('turbolinks:load',function(){
 
+  
   $(".hamburger").click(function(){
     $("#nav-hidden").toggle();
     $("header").toggleClass('open')
 
+  
+    //after burger click, close if outside click
     $('html').click(function(){
       $("#nav-hidden").hide();
       $("header").removeClass('open');
@@ -12,6 +16,11 @@ $(document).on('turbolinks:load',function(){
 
     $("header").click(function(e){
       e.stopPropagation();
+    });
+
+    $("header").find("#nav-hidden").click(function(e){
+      e.stopPropagation();
+      $(this).find("a").first().click();
     });
 
   });
