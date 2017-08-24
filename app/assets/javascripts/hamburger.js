3608be2b7d351b6da21957e3f567c57fbf79420b
Carson Rajcan <carajcan@gmail.com>
2017-08-23 21:25:27 -0500

    click outside nav to close on mobile test

--- app/assets/javascripts/hamburger.js
+++ app/assets/javascripts/hamburger.js
@@ -6,24 +6,15 @@ $(document).on('turbolinks:load',function(){
   });
 
 
-  $(document).click(function(){
+  $('html').click(function(){
     $("#nav-hidden").hide();
     $("header").removeClass('open');
   });
+
 
   $("header").click(function(e){
     e.stopPropagation();
   });
 
-/*
-  $('body').click(function(e){
-    if($(e.target).hasClass("open")){
-      return false;
-    } else {
-      $("#nav-hidden").toggle();
-      $("header").toggleClass('open');
-    }
-  });
-*/
 });
 
