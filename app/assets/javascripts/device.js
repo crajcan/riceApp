f5129719d62d143768a108bf4d265740b3306a16
Carson Rajcan <carajcan@gmail.com>
2017-08-24 00:41:07 -0500

    change mobile class to apply to html

--- app/assets/javascripts/device.js
+++ app/assets/javascripts/device.js
@@ -2,7 +2,7 @@ var classNames = [];
 if (navigator.userAgent.match(/(iPad|iPhone|iPod)/i)) classNames.push('device-ios');
 if (navigator.userAgent.match(/android/i)) classNames.push('device-android');
 
-var body = document.getElementsByTagName('body')[0];
+var html = document.getElementsByTagName('html')[0];
 
 if (classNames.length) classNames.push('on-device');
-if (body.classList) body.classList.add.apply(body.classList, classNames);
+if (html.classList) html.classList.add.apply(html.classList, classNames);
