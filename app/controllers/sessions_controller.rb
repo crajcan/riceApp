40fd5d779a404a860734ebffc9c4060aab6b3869
Carson Rajcan <carajcan@gmail.com>
2017-09-04 12:13:17 -0500

    fix some routing issues

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   
   def new
+    redirect_to '/home' if logged_in?
   end
  
   def create
@@ -8,7 +9,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       Log_in(@user)
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_to '/home'
     else
       flash.now[:danger] = 'Invalid email/password combination' 
       render 'new'
