2c635ba93972204b5014ffc761f68df9e0b8b648
Carson Rajcan <carajcan@gmail.com>
2017-09-07 00:11:53 -0500

    finish account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
 
-  def Log_in(user)
+  def log_in(user)
     session[:user_id] = user.id
   end
 
@@ -33,8 +33,8 @@ module SessionsHelper
       @current_user = @current_user || User.find_by(id: session[:user_id])
     elsif (user_id = cookies.signed[:user_id]) 
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        Log_in user
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
         @current_user = user
         end
     end
