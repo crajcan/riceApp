f3b306130974b8a86e1ae91bf3357e4de418a685
Carson Rajcan <carajcan@gmail.com>
2017-09-14 10:33:39 -0500

    change seed emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+             email: "email@#{APP_NAME}.org",
+             password:              "password",
+             password_confirmation: "password",
              activated: true,
              activated_at: Time.zone.now,
              admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@#{APP_NAME}.org"
   password = "password"
   User.create!(name:  name,
                email: email,
