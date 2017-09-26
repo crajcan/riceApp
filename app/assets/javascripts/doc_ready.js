aa1f8a1ee8a5019e37db6d8c7afcb4fc66db0527
Carson Rajcan <carajcan@gmail.com>
2017-09-26 18:12:39 -0500

    try changing targeting

--- app/assets/javascripts/doc_ready.js
+++ app/assets/javascripts/doc_ready.js
@@ -27,7 +27,7 @@ $(document).on('turbolinks:load',function(){
   //background image mobile
   $(window).scroll(function() {
     var scrolledY = $(window).scrollTop();
-    $('.on-device body').css('background-position', 'left ' + ((scrolledY)) + 'px');
+    $('.on-device > body').css('background-position', 'left ' + ((scrolledY)) + 'px');
   }); 
 
 });
