6bc57d9cd493cafbb96c997eb8676576c697a6e1
Carson Rajcan <carajcan@gmail.com>
2017-10-06 17:38:50 -0500

    clear flash on ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  after_action :clear_xhr_flash
 
   def front 
     redirect_to '/home' if logged_in?
@@ -15,5 +16,12 @@ class ApplicationController < ActionController::Base
         redirect_to login_url
       end
     end
+
+    def clear_xhr_flash
+      if request.xhr?
+        flash.discard
+      end
+    end
+
 
 end
