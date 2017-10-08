9aa0e326e3668bc3129a42a7065185a4b32bf2e9
Carson Rajcan <carajcan@gmail.com>
2017-10-08 01:50:52 -0500

    write test for ajax replies POST/DELETE

--- spec/requests/async_replies_spec.rb
+++ spec/requests/async_replies_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "AsyncReplies", type: :feature do
   describe "The Interface" do   
     fixtures :all
-
+    
     it "allows you to POST and DELETE replies with http" do
       @user = users(:archer)
       visit login_path
@@ -13,13 +13,16 @@ RSpec.describe "AsyncReplies", type: :feature do
       expect(page).to have_current_path('/home') 
       expect(page).not_to have_content('post reply')
       reply_opens = page.all(".reply-open") 
-      reply_opens.third.click
-      expect(page).to have_content('post reply')
+      reply_opens[2].click
+      expect(find_button('post reply').visible?).to be true
+      fill_in("reply[content]", :with => "This is my test reply")
+      click_button('post reply')
+      expect(page).to have_content('This is my test reply')
+      expect(page).to have_css('.alert-success')
+      expect(page).to have_content('Reply created')
     end
 
-
-    it "allows you to POST and DELETE replies with ajax" do
-  
+    it "allows you to POST and DELETE replies with ajax" do 
     end
 
   end
